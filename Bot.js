dd476eb42deb324a65ce13524ecec60947c2cffc
dj-monstercat <jordansim189189@gmail.com>
2015-04-26 03:44:22 -0500

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -211,7 +211,42 @@
                 ["mix", "You played a mix, which is against the rules. "],
                 ["sound", "The song you played had bad sound quality or no sound. "],
                 ["nsfw", "The song you contained was NSFW (image or sound). "],
-                ["unavailable", "The song you played was not available for some users. "]
+                ["unavailable", "The song you played was not available for some users. "],
+            ],
+            ball: [
+            "Signs point to yes.",
+            "Yes.",
+            "Reply hazy, try again.",
+            "Without a doubt.",
+            "My sources say no.",
+            "As I see it, yes.",
+            "You may rely on it.",
+            "Concentrate and ask again.",
+            "Outlook not so good.",
+            "It is decidedly so.",
+            "Better not tell you now.",
+            "Very doubtful.",
+            "Yes - definitely.",
+            "It is certain.",
+            "Cannot predict now.",
+            "Most likely.",
+            "Ask again later.",
+            "My reply is no.",
+            "Outlook good.",
+            "Don't count on it.",
+            "Yes, in due time.",
+            "My sources say no.",
+            "Definitely not.",
+            "You will have to wait.",
+            "I have my doubts.",
+            "Outlook so so.",
+            "Looks good to me!",
+            "Who knows?",
+            "Looking good!",
+            "Probably.",
+            "Are you kidding?",
+            "Don't bet on it.",
+            "Forget about it."
             ],
             afkpositionCheck: 15,
             afkRankCheck: "ambassador",
@@ -2931,11 +2966,29 @@
                     else {
                         if (typeof trapbot.settings.youtubeLink === "string")
                             API.sendChat(subChat(trapbot.chat.youtube, {name: chat.un, link: trapbot.settings.youtubeLink}));
+                    } 
+                }
+            },
+
+            ballCommand: {
+                command: ['8ball', 'ask'],
+                rank: 'user',
+                type: 'startsWith',
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!trapbot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                            var crowd = API.getUsers();
+                            var msg = chat.message;
+                            var argument = msg.substring(cmd.length + 1);
+                            var randomUser = Math.floor(Math.random() * crowd.length);
+                            var randomBall = Math.floor(Math.random() * trapbot.settings.ball.length);
+                            var randomSentence = Math.floor(Math.random() * 1);
+                            API.sendChat(subChat(trapbot.chat.ball, {name: chat.un, botname: trapbot.settings.botName, question: argument, response: trapbot.settings.ball[randomBall]}));
                     }
                 }
-            }
-        }
-    };
+           }
+       };
 
     loadChat(trapbot.startup);
 }).call(this);
